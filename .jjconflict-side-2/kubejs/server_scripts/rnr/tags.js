c6cbdcfa02c947ba69bd8b132e790a30712e27f6
Pyritie <pyritie@gmail.com>
2025-12-09 18:47:08 +0000

    well no wonder this wasn't working

--- .jjconflict-side-2/kubejs/server_scripts/rnr/tags.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const registerRnrItemTags = (event) => {
-
-	global.RNR_DISABLED_ITEMS.forEach(item => {
-		event.removeAllTagsFrom(item)
-		event.add('c:hidden_from_recipe_viewers', item)
-	});
-};

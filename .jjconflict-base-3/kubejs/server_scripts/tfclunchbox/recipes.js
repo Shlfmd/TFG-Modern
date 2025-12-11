4eb1a2154f871529586835f8cc996d9562ab34f7
Pyritie <pyritie@gmail.com>
2025-12-11 01:10:31 +0000

    improved tooltips, added boots

--- .jjconflict-base-3/kubejs/server_scripts/tfclunchbox/recipes.js
+++
@@ -1,9 +0,0 @@
-// priority: 0
-"use strict";
-
-function registerTFCLunchBoxRecipes(event) {
-    // Убираем оригинальные рецепты капсул
-    // Remove original capsule recipes
-    event.remove({ id: 'tfclunchbox:universal_capsule' })
-    event.remove({ id: 'tfclunchbox:fill_capsule_with_ice' })
-}

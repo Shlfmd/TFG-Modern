a3b115ece7506b329753c885623797a6307a4d9f
Epicificator <143143550+Epicificator@users.noreply.github.com>
2025-12-18 03:37:46 -0500

    New New belt textures! (#2454)
    * Natural rubber belt block textures
    
    Signed-off-by: Epicificator <143143550+Epicificator@users.noreply.github.com>
    
    * remove old styrene butadiene rubber belt block textures
    
    Signed-off-by: Epicificator <143143550+Epicificator@users.noreply.github.com>
    
    * New SBR belt textures!
    
    Signed-off-by: Epicificator <143143550+Epicificator@users.noreply.github.com>
    
    * Delete old rubber belt item texture
    
    Signed-off-by: Epicificator <143143550+Epicificator@users.noreply.github.com>
    
    * New belt item textures
    
    Signed-off-by: Epicificator <143143550+Epicificator@users.noreply.github.com>
    
    * Delete old silicone rubber belts
    
    Signed-off-by: Epicificator <143143550+Epicificator@users.noreply.github.com>
    
    * New silicone belt textures!
    
    Signed-off-by: Epicificator <143143550+Epicificator@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Epicificator <143143550+Epicificator@users.noreply.github.com>

--- .jjconflict-base-10/kubejs/client_scripts/main_client_script.js
+++
@@ -1,3 +0,0 @@
-ItemEvents.tooltip(evt => {
-    registerTooltips(evt);
-})
--- .jjconflict-side-11/kubejs/server_scripts/tfcbetterbf/data.js
+++
@@ -1,6 +0,0 @@
-// priority: 0
-"use strict";
-
-const registerTFCDataForTFCBetterBF = (event) => {
-    event.itemHeat('tfcbetterbf:insulation', 8.571, null, null)
-}
